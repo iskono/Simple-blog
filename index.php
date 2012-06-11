14cd210df8b3d678ab438f3a0e1917ecbce3ce8f
Angus Hammond <angusgh@gmail.com>
2012-06-11 18:31:04 +0100

    Initial commit
    Create homepage and simple stylesheet.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<?php
+$db=new SQLite3("data.db");
+$result=$db->query("SELECT * FROM posts ORDER BY time DESC");
+?>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="test.css" />
+		<title>Test page</title>
+	</head>
+	<body>
+		
+		<?php
+			while($row=$result->fetchArray())
+			{
+				echo "<div class=\"content\">";
+				echo "<h1>".$row["title"]."</h1>\n";
+				echo "<p>".$row["content"]."</p>\n";
+				echo "</div>";
+			}
+		?>
+		
+		<div id="side">
+			<ul>
+				<li> <a href="#1">one</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
