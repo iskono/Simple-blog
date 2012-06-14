ed7483e2aae8425f568c68f066a0f28d5ed90d11
Angus Hammond <angusgh@gmail.com>
2012-06-14 22:19:43 +0100

    Use prepared statements in handle_new_post.php

--- handle_new_post.php
+++ handle_new_post.php
@@ -3,10 +3,17 @@
 <?php
 
 $db=new SQLite3("data.db");
-$title=$db->escapeString($_POST["title"]);
-$content=$db->escapeString($_POST["content"]);
+$title=$_POST["title"];
+$content=$_POST["content"];
 
-$successful=$db->exec("INSERT INTO posts VALUES ('".$content."', '".$title."', strftime('%s', 'now'))");
+$stmt=$db->prepare("INSERT INTO posts VALUES (:content, :title, strftime('%s', 'now'))");
+
+$stmt->bindValue(":content", $content, SQLITE3_TEXT);
+$stmt->bindValue(":title", $title, SQLITE3_TEXT);
+
+$successful=$stmt->execute();
+
+//$successful=$db->exec("INSERT INTO posts VALUES ('".$content."', '".$title."', strftime('%s', 'now'))");
 
 if(!$successful)
 {
