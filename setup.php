24a6fc0f28006f64fdd658536b84259044005296
Angus Hammond <angusgh@gmail.com>
2012-06-15 13:07:48 +0100

    Add script to initialise database

---
+++ setup.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<?php
+	touch("data.db");
+
+	$db=new SQLite3("data.db");
+	$success=$db->exec("CREATE TABLE posts(content TEXT, title TEXT, time INTEGER);");
+
+	if(!$success)
+	{
+		die("Failed to create database");
+	}
+
+	$success=$db->exec("INSERT INTO posts VALUES ('This is just an example post', 'Hello, World!', strftime('%s', 'now'))");
+	if(!$success)
+	{
+		die("Failed to make example post to database");
+	}
+?>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>Database setup</title>
+	</head>
+	<body>
+		<div class="content">
+			<p>This functionality is still being implemented.</p>
+		</div>
+		<div id="side">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="insert.html">New post</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
